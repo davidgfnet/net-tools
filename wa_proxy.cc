633b9fd3d7820e5853bad4e468115672a159320f
David Guillen Fandos <david@davidgf.net>
2015-03-11 00:57:05 +0000

    Finished (sort of) the WA tool. Very bugy, probably will put your CPU at 100%. Meh, I don't really care as long as I can capture some traces.
    For future ref:
    ptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 5222
    
    Ands make your phone GW be your PC. Make sure you clean your iptables rules (unless you know what youre doing)

--- wa_proxy.cc
+++ wa_proxy.cc
@@ -28,6 +28,7 @@ struct tconn {
 	int fd[2];
 	int fdp[2];
 	struct sockaddr_in addr;
+	int wa_size;
 };
 
 
@@ -66,30 +67,176 @@ static int getdestaddr_iptables(int fd, const struct sockaddr_in *client, const 
 	return 0;
 }
 
+int wastringlen(unsigned char * buf, int count) {
+	if (buf[0] > 2 && buf[0] < 236) {
+		return 1;
+	}
+	
+	switch(buf[0]) {
+	case 236:
+		return 1 + 1;
+	case 250: {
+		int f = wastringlen(&buf[1], count-1);
+		return 1 + f + wastringlen(&buf[1+f], count-1-f);
+	}
+	case 252:
+		return 1 + 1 + buf[2];
+	case 253:
+		return 1 + 3 + ( (buf[3] << 16) | (buf[4] << 8) | (buf[5]));
+	case 255:
+		return 1 + 1 + (buf[1]&0x7f);
+	default:
+		printf("Error decoding string!\n");
+		return 0;
+	};
+}
+
+int wa_pkt_rewrite(unsigned char * buf, int count) {
+	// Return a -1 to skip rewriting!
+	printf("Flags %d\n", (int)buf[0]);
+
+	if (buf[0] != 0)
+		return -1;
+
+	if (count < 7)
+		return -1;
+
+	printf("Unencrypted packet\n");
+
+	// Only look at plain frames
+	int ptr = 3;
+	int lsize;
+	int lsizepos;
+	if (buf[ptr] == 0xf8) {
+		lsize = buf[ptr+1];
+		lsizepos = ptr+1;
+		ptr += 2;
+	}
+	else if (buf[ptr] == 0xf9) {
+		lsize = (buf[ptr+1] << 8) | buf[ptr+2];
+		lsizepos = ptr+2;
+		ptr += 3;
+	}
+	else
+		return -1;
+
+	if (buf[ptr++] != 12)
+		return -1;  // Only interested in auth packets!
+
+	printf("Got AUTH request from client!\n");
+
+	if (count < 8*16) return -1; // Would have a big blob of data
+
+	// Read & discard attributes
+	int numattr = (lsize - 2 + (lsize % 2)) / 2;
+	numattr *= 2;
+	while (numattr--) {
+		int strl = wastringlen(&buf[ptr], count);
+		printf("Skip string %d\n", strl);
+		ptr += strl;
+	}
+
+	if ((lsize & 1) == 1) {
+		printf("AUTH does not have data section!\n");
+		return -1;  // Does not have data blob
+	}
+
+	if (buf[ptr] == 0xf8 || buf[ptr] == 0xf9) {
+		printf("DATA is not string!\n");
+		return -1; // Has children instead of data blob
+	}
+
+	printf("AUTH seems to have a data blob!\n");
+
+	// Remove the last bytes of the packet and set a 1
+	// to remove the data section
+	int new_size = ptr;
+	buf[lsizepos] --;
+
+	int msgsize = ptr-3;
+
+	buf[1] = msgsize >> 8;
+	buf[2] = msgsize & 0xFF;
+
+	return new_size;
+}
+
+int wa_pktsize(tconn * c, int channel) {
+	//printf("pktsize 1\n");
+	if (c->buffers_len[channel] < 3)
+		return 0;
+	//printf("pktsize 2\n");
+	if (memcmp(c->buffers[channel], "WA", 2) == 0 &&
+		c->buffers_len[channel] >= 4)
+		return 4;
+
+	//printf("pktsize\n");
+	
+	unsigned char flags = c->buffers[channel][0];
+	unsigned short pktsize = (c->buffers[channel][1] << 8) | c->buffers[channel][2];
+
+	if (c->buffers_len[channel] >= pktsize + 3) {
+		printf("WA packet size: %d\n", pktsize+3);
+		int ns = wa_pkt_rewrite(c->buffers[channel], pktsize+3);
+
+		if (ns < 0) return pktsize + 3;
+
+		memmove(&c->buffers[channel][ns], &c->buffers[channel][pktsize+3], c->buffers_len[channel] - ns);
+		c->buffers_len[channel] -= (pktsize+3 - ns);
+		
+		return ns;
+	}
+
+	//printf("pktsize 0\n");
+	return 0;
+}
+
+int wa_haspkt(tconn * c, int channel) {
+	if (c->status != stWA || channel == 1) {
+		return c->buffers_len[channel];
+	}else if (c->status == stWA) {
+		//printf("Client->Server %d %d\n", c->buffers_len[channel], c->wa_size);
+		if (c->wa_size == 0)
+			c->wa_size = wa_pktsize(c, channel);
+
+		return c->wa_size;
+	}
+}
+
 int data_fwd(int channel, tconn * c, int wadetect = 0) {
-	if (c->buffers_len[channel] > 0) {
-		int w = write(c->fd[channel ^ 1], c->buffers[channel], c->buffers_len[channel]);
+	// Only client to server packet rewriting
+	int wsize = wa_haspkt(c, channel);
+
+	if (wsize > 0) {
+		int w = write(c->fd[channel ^ 1], c->buffers[channel], wsize);
 		if (w >= 0) {
-			printf("Transferring data... %d\n", w);
+			if (w > 0) printf("Transferring data... %d\n", w);
 			memmove(&c->buffers[channel][0], &c->buffers[channel][w], c->buffers_len[channel] - w);
 			c->buffers_len[channel] -= w;
+			if (c->wa_size > 0)
+				c->wa_size -= w;
 		}
 		else if (!WOULDBLOCK)
 			return -1;
 	}
-	if (c->buffers_len[channel] == 0) {
-		int r = read(c->fd[channel], c->buffers[channel], FWD_BUFFER_SIZE);
+	if (c->buffers_len[channel] < FWD_BUFFER_SIZE) {
+		int r = read(c->fd[channel], c->buffers[channel], FWD_BUFFER_SIZE - c->buffers_len[channel]);
 		if (r > 0) {
 			if (wadetect && c->status == stNew) {
 				if (c->buffers[channel][0] == 'W' && 
-					c->buffers[channel][1] == 'A')
-				c->status = stWA;
-				printf("WA gotcha!\n");
+					c->buffers[channel][1] == 'A') {
+
+					c->status = stWA;
+					printf("WA gotcha!\n");
+				}
 			}
 			else
-				c->status = stNonWA;
+				if (c->status == stNew) {
+					printf("NONWA\n");
+					c->status = stNonWA;
+				}
 			c->buffers_len[channel] = r;
-			printf("Receiving data... %d\n", r);
+			//printf("Receiving data... %d\n", r);
 		}
 		else if (!WOULDBLOCK)
 			return -1;
@@ -159,6 +306,7 @@ int main() {
 				cs[idx].buffers_len[1] = 0;
 				cs[idx].buffers[0] = (unsigned char*)malloc(FWD_BUFFER_SIZE);
 				cs[idx].buffers[1] = (unsigned char*)malloc(FWD_BUFFER_SIZE);
+				cs[idx].wa_size = 0;
 
 				num_active_clients++;
 				printf("New connection!\n");
@@ -212,7 +360,7 @@ int main() {
 			if (cs[i].status == stConnecting || 
 				(cs[i].status != stUnused && cs[i].buffers_len[1] > 0) )
 				fdtable[cs[i].fdp[0]].events |= POLLOUT;
-			if ((cs[i].status != stUnused && cs[i].buffers_len[0] > 0) )
+			if ((cs[i].status != stUnused && wa_haspkt(&cs[i],0) > 0) )
 				fdtable[cs[i].fdp[1]].events |= POLLOUT;
 
 			for (int i = 0; i < 2; i++)
